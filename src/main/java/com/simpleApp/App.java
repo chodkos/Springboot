24a6928f352c239adf7c565eb4577465c73d9f92
Raynor <chodkos@gmail.com>
2017-10-16 13:36:12 +0200

    greeting added

--- src/main/java/com/simpleApp/App.java
+++ src/main/java/com/simpleApp/App.java
@@ -3,10 +3,18 @@ package com.simpleApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class App {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String helloWorld(){
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class);
